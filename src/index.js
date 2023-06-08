abb3168b2eda24d20ac53299038f0bb2bcbe0fc4
muta-jonathan <mutajonathan4@gmail.com>
2023-06-08 18:21:08 +0300

    Refactor to custom component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { faker } from '@faker-js/faker';
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
     return (
     <React.Fragment>
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar"  src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                   <a href="/" className="author">Sam</a>
-                   <div className="metadata">
-                    <span className="date"> Today at 7:00PM </span>
-                    </div> 
-                    <div className="text">
-                        Nice blog post
-                    </div>
-                </div>
-            </div>
+           <CommentDetail />
+           <CommentDetail />
+           <CommentDetail />
+           <CommentDetail />
+           <CommentDetail />
         </div>
     </React.Fragment>
     );
